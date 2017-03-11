5600c54a4afb67f5bc85f953ba387d3b0a4b13d9
geraldo <geraldo@servus.at>
2017-03-11 23:14:03 +0100

    add proxy

--- odooapp/views.py
+++ odooapp/views.py
@@ -1,15 +1,23 @@
 from django.http import HttpResponse
-import xmlrpclib
+import xmlrpclib, httplib
+from odooapp.proxy import *
 
 url = "https://demo3.odoo.com"
-db = "demo_100_1489044950"
+db = "demo_100_1489265126"
 username = "admin"
 password = "admin"
 
 #odoo service connection
-common = xmlrpclib.ServerProxy('{}/xmlrpc/2/common'.format(url))
+#info = xmlrpclib.ServerProxy('https://demo.odoo.com/start').start()
+#print info
+
+p = ProxiedTransport()
+p.set_proxy('proxy.server:3128')
+
+common = xmlrpclib.ServerProxy('{}/xmlrpc/2/common'.format(url), transport = p)
 uid = common.authenticate(db, username, password, {})
-models = xmlrpclib.ServerProxy('{}/xmlrpc/2/object'.format(url))
+
+models = xmlrpclib.ServerProxy('{}/xmlrpc/2/object'.format(url), transport = p)
 products = models.execute_kw(db, uid, password,
     'product.template', 'search_read',
     [[]],
